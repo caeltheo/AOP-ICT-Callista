577c21cadfb6485ebbbb1a5d6ed5fcacb386de08
caeltheo <cltheorine@gmail.com>
2025-04-28 12:50:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function tampil() {
+    document.getElementById("Alert").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function Tutup() {
+    document.getElementById("Alert").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
